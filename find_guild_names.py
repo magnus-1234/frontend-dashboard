524daeef14abd42af4d5cd1bf27a71922934f773
storage2mohitraj-ui <storage2mohitraj@gmail.com>
2026-05-04 02:34:55 +0530

    fix: restructure repository to move dashboard files to root and resolve 404 error

--- find_guild_names.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-client = pymongo.MongoClient('mongodb+srv://iammagnusx1_db_user:zYFHUOjjXhfGLpMs@reminder.hlx5aem.mongodb.net/?appName=REMINDER')
-db = client['reminderbot']
-print("From auto_redeem_settings:")
-for g in db['auto_redeem_settings'].find():
-    print(f"Guild ID: {g.get('guild_id')}, Name: {g.get('guild_name', 'Unknown')}")
